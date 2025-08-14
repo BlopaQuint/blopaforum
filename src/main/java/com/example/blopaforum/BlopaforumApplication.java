54e22ba2728bb324c65747017d31bf67b9d5af66
Pablo Quintiliano <pabloquintiliano@live.com>
2025-08-14 01:35:32 -0300

    Setup inicial do projeto Spring Boot

---
+++ src/main/java/com/example/blopaforum/BlopaforumApplication.java
@@ -0,0 +1,13 @@
+package com.example.blopaforum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlopaforumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlopaforumApplication.class, args);
+	}
+
+}
